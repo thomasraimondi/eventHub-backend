57395d6e97e08a7423abc1d35181e824298a9ddf
thomasraimondi <thomas.raimondi14@gmail.com>
2025-10-21 12:24:42 +0200

    add register user feature

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const authController = require("../controllers/authController");
 const { checkLogin } = require("../middleware/auth/checkLogin");
 const { validationDataLogin } = require("../middleware/auth/validationDataLogin");
+const { validationDataRegister } = require("../middleware/auth/validationDataRegister");
 
 router.post("/login", validationDataLogin, checkLogin, authController.login);
-router.post("/register", authController.register);
+router.post("/register", validationDataRegister, authController.register);
 
 module.exports = router;
