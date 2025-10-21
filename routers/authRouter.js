c771347ef846dca4438dc926d6eedd9b7b01c4fb
thomasraimondi <thomas.raimondi14@gmail.com>
2025-10-21 15:40:58 +0200

    add update profile and change password feature

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -4,9 +4,10 @@ const authController = require("../controllers/authController");
 const { checkLogin } = require("../middleware/auth/checkLogin");
 const { validationDataLogin } = require("../middleware/auth/validationDataLogin");
 const { validationDataRegister } = require("../middleware/auth/validationDataRegister");
+const { checkPassword } = require("../middleware/Validation/checkPassword");
 
 router.post("/login", validationDataLogin, checkLogin, authController.login);
 router.get("/logout", authController.logout);
-router.post("/register", validationDataRegister, authController.register);
+router.post("/register", validationDataRegister, checkPassword, authController.register);
 
 module.exports = router;
